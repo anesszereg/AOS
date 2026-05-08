5cdb10b8e2e3e76f92dc36db31d7a599357e10f2
Anesszereg <anesszereg1@gmail.com>
2026-05-08 23:37:36 +0100

    update

--- frontend/food-delivery-app/src/services/api.ts
+++ frontend/food-delivery-app/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://food-delevery-app-g73l.onrender.com/api';
+const API_BASE_URL = 'http://localhost:8000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
